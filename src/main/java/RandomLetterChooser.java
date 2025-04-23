3c4b2c23982f11798b4c599707eef212c3d1d7ef
winstonh1 <wihuang2@s.sfusd.edu>
2025-04-23 09:53:13 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -3,7 +3,7 @@ public class RandomLetterChooser extends RandomStringChooser
 	
 	public RandomLetterChooser(String str)
 	{ 
-		/* to be implemented in part (b) */
+		super(getSingleLetters(str));
 		
 	}
 	
